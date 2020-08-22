6928d79808c305162f2e9229f71ef291094ebbd8
Umut <av.umutgungor92@gmail.com>
2020-08-22 11:51:58 +0200

    day04 ve day05 ders sonu

--- src/com/techproed/Day04LocatorsGiris.java
+++ src/com/techproed/Day04LocatorsGiris.java
@@ -7,47 +7,21 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
-public class Day03_LocatorsGiris {
-
+public class Day04LocatorsGiris {
     public static void main(String[] args) {
-
         System.setProperty("webdriver.chrome.driver","C:\\Users\\avumu\\Documents\\selenium dependencies\\drivers\\chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(3000, TimeUnit.MILLISECONDS);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         driver.get("http://a.testaddressbook.com/");
-
-        //ilk webelement'imizi buluyoruz.
-        //Web sayfasindaki tum elemanlar -> WebElement== Non-Primitive Data Type
-
-        //webelement'imize tikladik
-        WebElement signInLink  = driver.findElement(By.id("sign-in"));
+        WebElement signInLink = driver.findElement(By.id("sign-in"));
         signInLink.click();
-
-        //e mail kutusunu bulalim
-        WebElement emailKutusu =driver.findElement(By.id("session_email"));
+        WebElement emailKutusu = driver.findElement(By.className("form-control"));
         emailKutusu.sendKeys("testtechproed@gmail.com");
-
-        //password kutusunu bulalim
         WebElement sifreKutusu = driver.findElement(By.id("session_password"));
-
-        //sifre kutusunun icersine yazi gonderiyoruz
         sifreKutusu.sendKeys("Test1234!");
-
-        //sign in butonunu buluyoruz
         WebElement signInButonu = driver.findElement(By.name("commit"));
         signInButonu.click();
-
-        String baslik = driver.getTitle();
-        if (baslik.equals("Address Book")){
-            System.out.println("Giris Basarili");
-        }else {
-            System.out.println("Giris Basarisiz");
-        }
-
-
-    driver.quit();
-
     }
 }
